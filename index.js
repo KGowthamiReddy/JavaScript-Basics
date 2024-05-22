71d33ebbdb23d2c1a96cc2b19245e17275d8ab17
Gowthami Reddy <gowthamik710@gmail.com>
2024-05-22 14:32:55 +0530

    add : javascript basics

---
+++ index.js
@@ -0,0 +1,23 @@
+// JS statements
+let x, y, z;        // variable declaration
+x = 5;              // initialization of a variable
+y = 10;
+z = x + y;          // perform addition of a variables
+console.log(z);     // 15 print the results in console
+
+// JS Comments
+let a = 10;
+// a = 1;       single lime comments
+
+// multi-line comments
+/*Using comments to 
+prevent execution of code is 
+suitable for code testing. */
+
+// JS Variables (let, const, var)
+const bat = 2;
+const ball = 1;
+let cricket = bat + ball;
+console.log(cricket);   // 3
+
+
